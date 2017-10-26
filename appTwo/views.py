e73889e84656f0b8cbd18ebc5cb5629816e4ecac
williamdelrosario <williamdelrosario@yahoo.com>
2017-10-27 01:08:43 +0200

    Model Forms

--- appTwo/views.py
+++ appTwo/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
-from appTwo.models import User
-'''from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request):
-    return HttpResponse("<em>My Second Project!</em>")
-
-def help(request):
-    helpdict = {'help_insert':'HELP PAGE'}
-    return render(request,'apptwo/help.html',context=helpdict)'''
-
+#from django.http import HttpResponse
+from appTwo.forms import NewUserForm
+#from appTwo.models import User
 
 def index(request):
     return render(request,'appTwo/index.html')
 
 def users(request):
 
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users':user_list}
-    return render(request, 'appTwo/users.html',context=user_dict)
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return idex(request)
+    else:
+        print('ERROR FROM INVALID')
+
+return render(request,'appTwo/users.html,{'form':form})
